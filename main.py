f539930f266cca639f67328294d210f0f6f5989c
jeser <jeserchivekalt@gmail.com>
2026-05-30 12:33:52 -0300

    Implementação final da documentação do swagger

--- main.py
+++ main.py
@@ -1,15 +1,46 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Request, Query
 from datetime import date
 import requests
+from fastapi.exceptions import RequestValidationError
+from pydantic import BaseModel
+from starlette.responses import JSONResponse
 
 app = FastAPI(docs_url="/docs", redoc_url="/redoc")
+
+class ApiResponse(BaseModel):
+    data: date
+    conteudo: object
+    mensagem: str
+
+class InvalidParameterException(BaseModel):
+    mensagem: str
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=422,
+        content={"mensagem": "Parâmetro usado é inválido ou não está no formato YYYY-MM-DD"}
+    )
 
 @app.get(
     "/",
     tags=["Rotas"],
     response_description="Dia enviado como parâmetro (ou o dia de hoje) + Lista Recebida da API externa",
+    response_model=ApiResponse,
+    responses={
+        422: {"model": InvalidParameterException, "description": "Invalid parameter provided"},
+    },
 )
-async def root(dia: date | None = None):
+async def root(
+        dia: Annotated[
+            date | None,
+            Query(
+                alias="data",
+                description="Data no formato YYYY-MM-DD",
+            )
+        ] = None,
+) -> ApiResponse:
 
     if dia is None:
         dia = date.today()
@@ -17,13 +48,15 @@ async def root(dia: date | None = None):
     request_externa = requests.get("https://testedefensoriapr.pythonanywhere.com/precos")
 
     if request_externa.status_code != 200:
-        return {
-            "data": dia,
-            "mensagem": "Não foi possível consumir a API externa :("
-        }
+        return ApiResponse(
+            data=dia,
+            conteudo="[]",
+            mensagem="Não foi possível consumir a API externa :("
+        )
+
     else:
-        return {
-            "data": dia,
-            "conteudo": request_externa.json(),
-            "mensagem": "API externa consumida com sucesso :)"
-        }
+        return ApiResponse(
+            data=dia,
+            conteudo=request_externa.json(),
+            mensagem="API externa consumida com sucesso :)"
+        )
